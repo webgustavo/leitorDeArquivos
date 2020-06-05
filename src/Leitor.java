5fb8b964e2dfb1ebebb2fc868491c526ee1a32a3
Carlos Vieira <web.gustavo@hotmail.com>
2020-06-05 14:46:37 -0300

    Initial commit

---
+++ src/Leitor.java
@@ -0,0 +1,17 @@
+import java.io.FileInputStream;
+
+public class Leitor {
+
+    public static void main(String args[]) {
+
+        try {
+            FileInputStream arquivo = new FileInputStream("c:/arquivos/teste.txt");
+            //System.out.println("ok");
+
+
+
+        } catch (Exception e) {
+
+        }
+    }
+}
